1526812abac46affeaad8d8b2a4448c3a0a8565f
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 14:20:50 +0200

    'Update'

--- MeanFilter/scripts/MeanFilter.lua
+++ MeanFilter/scripts/MeanFilter.lua
@@ -1,31 +1,3 @@
---[[----------------------------------------------------------------------------
-
-  Application Name: 
-  MeanFilter                                                                                                                      
-               
-  Summary 
-  Applying mean filter and echo filter on scans read from a file    
-                                                                         
-  Description:
-  This sample shows how to apply a mean filter and an echo filter to scans read 
-  from file and displays the filtered scan as a point cloud. A file scan provider is 
-  created and and the scans from the file are played back. The filtered scan is 
-  shown in the PointCloud viewer on the webpage.
-  In addition, the noise is estimated as positive difference between the mean distances 
-  and the distances of the last scan of a block.
-  
-  How to run:
-  Starting this sample is possible either by running the app (F5) or 
-  debugging (F7+F10). Output is printed to the console and the transformed 
-  point cloud can be seen on the viewer in the web page. The playback stops 
-  after the last scan in the file. To replay, the sample must be restarted.
-  To run this sample, a device with AppEngine >= 2.5.0 is required.
-  
-  Implementation: 
-  To run with real device data, the file provider has to be exchanged with the 
-  appropriate scan provider.
-  
-------------------------------------------------------------------------------]]
 
 --Start of Global Scope--------------------------------------------------------- 
 local scanCounter = 0
@@ -134,7 +106,7 @@ function handleNewScan(scan)
     
     -- Transform to PointCloud to view in the PointCloud viewer on the webpage
     local pointCloud = Scan.Transform.transformToPointCloud(transform, filteredScan)
-    View.add(viewer, pointCloud)
+    View.addPointCloud(viewer, pointCloud)
     View.present(viewer)
   end
 end
--- readme.md
+++ readme.md
@@ -1,9 +1,7 @@
 ## MeanFilter
-
 Applying mean filter and echo filter on scans read from a file
 
-### Description
-
+### Description:
 This sample shows how to apply a mean filter and an echo filter to scans read
 from file and displays the filtered scan as a point cloud. A file scan provider is
 created and and the scans from the file are played back. The filtered scan is
@@ -11,11 +9,7 @@ shown in the PointCloud viewer on the web-page.
 In addition, the noise is estimated as positive difference between the mean distances
 and the distances of the last scan of a block.
 
-### How to Run
-
-This sample may currently be outdated.
-Editing the UI might not work properly in the latest version of SICK AppStudio. In order to edit the UI, you can either use SICK AppStudio version <= 2.4.2 or recreate it within the current version of SICK AppStudio by using the ScanView element from the extended control library (available for download).
-
+### How To Run:
 Starting this sample is possible either by running the App (F5) or
 debugging (F7+F10). Output is printed to the console and the transformed
 point cloud can be seen on the viewer in the web page. The playback stops
@@ -23,7 +17,6 @@ after the last scan in the file. To replay, the sample must be restarted.
 To run this sample, a device with AppEngine >= 2.5.0 is required.
 
 ### Implementation
-
 To run with real device data, the file provider has to be exchanged with the
 appropriate scan provider.
 
